f32a2f809304060519148c3c14c755c478ec583c
Nóra Szepes <nora.szepes@tenforce.com>
2017-03-20 16:33:54 +0100

    showing list of docker files - added sorting on title too

--- app/routes/compact.js
+++ app/routes/compact.js
@@ -1,4 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model: function(){
+    return this.get('store').findAll('docker-compose').then((dc) => {
+      return dc.sortBy('title');
+    });
+  }
 });
