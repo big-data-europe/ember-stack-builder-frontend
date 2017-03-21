5877cb34d5943f2faca284fabe1c5f5a84353616
Nóra Szepes <nora.szepes@tenforce.com>
2017-03-21 15:45:19 +0100

    moved the search task from the controller to the route. debounce value is in the environment.js

--- app/controllers/compact.js
+++ app/controllers/compact.js
@@ -1,26 +1,7 @@
 import Ember from 'ember';
-import { task, timeout } from 'ember-concurrency';
 
-
-const DEBOUNCE_MS = 250;
 export default Ember.Controller.extend({
   searchValue: "",
-
-  dockerList: task(function*(term) {
-    if (Ember.isBlank(term)) {
-      this.set('searchValue', "");
-      return;
-    }
-
-    // Pause here for DEBOUNCE_MS milliseconds. Because this
-    // task is `restartable`, if the user starts typing again,
-    // the current search will be canceled at this point and
-    // start over from the beginning. This is the
-    // ember-concurrency way of debouncing a task.
-    yield timeout(DEBOUNCE_MS);
-    this.set('searchValue', term);
-    return;
-  }).restartable(),
 
   actions: {
     addNewFile: function() {
--- app/routes/compact.js
+++ app/routes/compact.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 import { task, timeout } from 'ember-concurrency';
+import env from '../config/environment';
 
 export default Ember.Route.extend({
   queryParams: {
@@ -8,11 +9,21 @@ export default Ember.Route.extend({
     }
   },
 
+  // this is to have the first debounce at 0, so then it is instant
+  debounce: 0,
+  //  here we return the task itself, so in the hbs we can see
+  // if it's still running or not
   model: function(params) {
-    return this.get('filterDockerList').perform(params.searchValue);
+    this.get('filterDockerList').perform(params.searchValue);
+    return this.get('filterDockerList');
   },
 
   filterDockerList: task(function*(searchValue) {
+
+    // first we debounce with 0
+    // then we will use it from the settings in the environment.js
+    yield timeout(this.get('debounce'));
+    this.set('debounce', env.searchDebounceMiliseconds);
     var params = {
       sort: 'title'
     };
@@ -21,6 +32,6 @@ export default Ember.Route.extend({
         title: searchValue
       };
     }
-    return this.get('store').query('docker-compose', params);
+    return yield this.get('store').query('docker-compose', params);
   }).restartable(),
 });
