5353e5c82bce6b82aa5f8535a97d1560a433ccec
Nóra Szepes <nora.szepes@tenforce.com>
2017-03-21 14:33:46 +0100

    added ember concurrency. search on docker compose now works
    Using a task and only using the model if the search field is empty. Debounce is set to 250ms.
    Also added design for it

--- app/controllers/compact.js
+++ app/controllers/compact.js
@@ -1,6 +1,35 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
 
+
+const DEBOUNCE_MS = 250;
 export default Ember.Controller.extend({
+
+  dockerList: task(function*(term) {
+    if (Ember.isBlank(term)) {
+      return this.get('model');
+    }
+
+    // Pause here for DEBOUNCE_MS milliseconds. Because this
+    // task is `restartable`, if the user starts typing again,
+    // the current search will be canceled at this point and
+    // start over from the beginning. This is the
+    // ember-concurrency way of debouncing a task.
+    yield timeout(DEBOUNCE_MS);
+
+    return yield this.filterDockerList(term);
+  }).on('init').restartable(),
+
+  filterDockerList: function(searchValue) {
+    var params = {
+      sort: 'title',
+      filter: {
+        title: searchValue
+      }
+    };
+    return this.get('store').query('docker-compose', params);
+  },
+
   actions: {
     addNewFile: function() {
       console.log("new");
