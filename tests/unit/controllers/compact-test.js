49fbe6e7be2df99f9df64391b3ef7b04824c38bb
Nóra Szepes <nora.szepes@tenforce.com>
2017-03-21 12:52:11 +0100

    cleaned compact controller and added searchbar

---
+++ tests/unit/controllers/compact-test.js
@@ -0,0 +1,12 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor('controller:compact', 'Unit | Controller | compact', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+});
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
