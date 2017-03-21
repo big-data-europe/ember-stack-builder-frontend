11abe7b5cadf369fd055a90f96a2b2cd07f7725b
Nóra Szepes <nora.szepes@tenforce.com>
2017-03-21 14:33:22 +0100

    from frontend sorting went to backend sorting for docker compose model

--- app/routes/compact.js
+++ app/routes/compact.js
@@ -2,8 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function(){
-    return this.get('store').findAll('docker-compose').then((dc) => {
-      return dc.sortBy('title');
-    });
+    var params = {
+      sort: 'title'
+    };
+    return this.get('store').query('docker-compose', params);
   }
 });
