58fe7876fe318a8b997318e5b11c949e11e1799a
Nóra Szepes <nora.szepes@tenforce.com>
2017-04-11 14:44:51 +0200

    added default values for new docker compose file

--- app/routes/editor/index.js
+++ app/routes/editor/index.js
@@ -2,6 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model() {
-    return this.get('store').createRecord('docker-compose');
+    return this.get('store').createRecord('docker-compose',{
+      text: 'version: "2"\n  services:\n    ',
+      title: 'New Docker Compose file'
+    });
   }
 });
